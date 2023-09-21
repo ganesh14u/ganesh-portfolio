c774401777c535254528e451ed29473d45ca9f20
ganesh14u <59154299+ganesh14u@users.noreply.github.com>
2023-09-21 20:15:18 +0530

    Update script.js
    Form data submit

--- script.js
+++ script.js
@@ -16,3 +16,14 @@ let typed = new Typed('.auto-input',{
     backDelay: 1000,
     loop: true,
 })
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxkgWotCMyvIKoT9aGtPCndZdHtHrq8i972t9CKAVjO6xC7T0I/exec'
+            const form = document.forms['google-sheet']
+          
+            form.addEventListener('submit', e => {
+              e.preventDefault()
+              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+                .catch(error => console.error('Error!', error.message))
+            })
